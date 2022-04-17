0e4a4c173d404e698fae3b2941071613c82c2751
Jeraldine Milla <jeraldinemilla@gmail.com>
2022-04-17 11:10:27 -1000

    Added test cases

--- app/tests/navbar.component.js
+++ app/tests/navbar.component.js
@@ -36,6 +36,11 @@ class NavBar {
     await testController.click('#login-dropdown');
     await testController.click('#login-dropdown-sign-up');
   }
+
+  /** Pull down login menu, go to list stuff page. */
+  async gotoListStuffPage(testController) {
+    await testController.click('#navbar-list-stuff');
+  }
 }
 
 export const navBar = new NavBar();
